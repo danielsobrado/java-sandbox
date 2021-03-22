593a099f935179ecef9c6557e093d4139f360dbb
Daniel <daniel@dalamarblog.com>
2021-03-22 20:22:55 +0400

    update fpe udf

--- format-preserving-encryption/src/main/java/com/jds/fpe/Encryption.java
+++ format-preserving-encryption/src/main/java/com/jds/fpe/Encryption.java
@@ -1,5 +1,177 @@
 package com.jds.fpe;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.idealista.fpe.FormatPreservingEncryption;
+import com.idealista.fpe.builder.FormatPreservingEncryptionBuilder;
+import com.idealista.fpe.config.Domain;
+import com.idealista.fpe.config.GenericDomain;
+import com.idealista.fpe.config.GenericTransformations;
+import com.idealista.fpe.transformer.IntToTextTransformer;
+import com.idealista.fpe.transformer.TextToIntTransformer;
+import com.jds.fpe.alphabet.CapitalAlphabet;
+import com.jds.fpe.alphabet.CompleteAlphabet;
+import com.jds.fpe.alphabet.NumbersAlphabet;
+
 public class Encryption {
+
+	public static final NumbersAlphabet NUMBERS_ALPHABET = new NumbersAlphabet();
+	public static final CompleteAlphabet ALL_LETTERS_ALPHABET = new CompleteAlphabet();
+	public static final CapitalAlphabet CAP_LETTERS_ALPHABET = new CapitalAlphabet();
+    static final TextToIntTransformer TEXT_TO_INT_TRANSFORMER = new GenericTransformations(NUMBERS_ALPHABET.availableCharacters());
+    static final IntToTextTransformer INT_TO_TEXT_TRANSFORMER = new GenericTransformations(NUMBERS_ALPHABET.availableCharacters());
+    static final TextToIntTransformer ALL_TEXT_TO_INT_TRANSFORMER = new GenericTransformations(ALL_LETTERS_ALPHABET.availableCharacters());
+    static final IntToTextTransformer INT_TO_ALL_TEXT_TRANSFORMER = new GenericTransformations(ALL_LETTERS_ALPHABET.availableCharacters());
+    static final TextToIntTransformer CAP_TEXT_TO_INT_TRANSFORMER = new GenericTransformations(CAP_LETTERS_ALPHABET.availableCharacters());
+    static final IntToTextTransformer INT_TO_CAP_TEXT_TRANSFORMER = new GenericTransformations(CAP_LETTERS_ALPHABET.availableCharacters());
+    static final Domain NUMBERS_DOMAIN = new GenericDomain(new NumbersAlphabet(), TEXT_TO_INT_TRANSFORMER, INT_TO_TEXT_TRANSFORMER);
+    static final Domain ALL_LETTERS_DOMAIN = new GenericDomain(new CapitalAlphabet(), ALL_TEXT_TO_INT_TRANSFORMER, INT_TO_ALL_TEXT_TRANSFORMER);
+    static final Domain CAP_LETTERS_DOMAIN = new GenericDomain(new CapitalAlphabet(), CAP_TEXT_TO_INT_TRANSFORMER, INT_TO_CAP_TEXT_TRANSFORMER);
+	
+    static byte[] keyByteArrray = new byte[]{
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01,
+            (byte) 0x02, (byte) 0x02, (byte) 0x02, (byte) 0x02,
+            (byte) 0x03, (byte) 0x03, (byte) 0x03, (byte) 0x03
+    }; 
+    
+    static byte[] anyTweak = new byte[]{
+            (byte) 0x01, (byte) 0x03, (byte) 0x02, (byte) 0x04
+    };
+    
+    public static Integer encryptInt(Integer input) {
+    	
+        FormatPreservingEncryption numbersFormatPreservingEncryption = FormatPreservingEncryptionBuilder
+                .ff1Implementation()
+                .withDomain(NUMBERS_DOMAIN)
+                .withDefaultPseudoRandomFunction(keyByteArrray)
+                .withDefaultLengthRange()
+                .build();
+        
+        // Min lenght is 2 characters
+        if (input < 10) {
+        	input = input + 10;
+        }
+        
+        String cipherText = numbersFormatPreservingEncryption.encrypt(input.toString(), anyTweak);
+        
+		return Integer.parseInt(cipherText);
+    	    	
+    }
+
+    public static String encryptStr(String input) {
+    	
+        FormatPreservingEncryption defaultFormatPreservingEncryption1 = FormatPreservingEncryptionBuilder.ff1Implementation()
+                .withDefaultDomain()
+                .withDefaultPseudoRandomFunction(keyByteArrray)
+                .withDefaultLengthRange()
+                .build();
+        
+        FormatPreservingEncryption numbersFormatPreservingEncryption = FormatPreservingEncryptionBuilder
+                .ff1Implementation()
+                .withDomain(NUMBERS_DOMAIN)
+                .withDefaultPseudoRandomFunction(keyByteArrray)
+                .withDefaultLengthRange()
+                .build();
+        
+        FormatPreservingEncryption allLettersFormatPreservingEncryption = FormatPreservingEncryptionBuilder
+                .ff1Implementation()
+                .withDomain(ALL_LETTERS_DOMAIN)
+                .withDefaultPseudoRandomFunction(keyByteArrray)
+                .withDefaultLengthRange()
+                .build();
+        
+        FormatPreservingEncryption capLettersFormatPreservingEncryption = FormatPreservingEncryptionBuilder
+                .ff1Implementation()
+                .withDomain(CAP_LETTERS_DOMAIN)
+                .withDefaultPseudoRandomFunction(keyByteArrray)
+                .withDefaultLengthRange()
+                .build();
+    	
+        HashMap<Integer, Character> othersMap = new HashMap<Integer, Character>(); 
+        StringBuilder numbers = new StringBuilder();
+        Queue<Character> queueOthers = new LinkedList<Character>();
+        StringBuilder originalLetters = new StringBuilder();
+        StringBuilder lowerLetters = new StringBuilder();
+        StringBuilder capLetters = new StringBuilder();
+        
+        for (int i = 0; i < input.length(); i++){
+        	Character currChar = input.charAt(i);
+            if ((Character.isLetter(currChar)) && (Character.isLowerCase(currChar))) {
+            	lowerLetters.append(currChar);
+            	originalLetters.append(currChar);
+            } else if ((Character.isLetter(currChar)) && (!Character.isLowerCase(currChar))) {
+            	capLetters.append(currChar);
+            	originalLetters.append(currChar);
+            } else if (Character.isDigit(currChar)) {
+            	numbers.append(currChar);
+            } else {
+            	othersMap.put(i, currChar);
+            	queueOthers.add(currChar);
+            }
+        }
+        
+        System.out.println("Original Numbers: " + numbers);
+        System.out.println("Original Letters: " + originalLetters);
+        System.out.println("Original Cap Letters: " + capLetters);
+        System.out.println("Original Lower Letters: " + lowerLetters);
+        
+        String numbersEnc;
+        if (numbers.length() > 1) {
+        	numbersEnc = numbersFormatPreservingEncryption.encrypt(numbers.toString(), anyTweak);
+        } else {
+        	numbersEnc = numbers.toString();
+        }
+        
+        String lowerLettersEnc;
+        if (lowerLetters.length() > 1) {
+        	lowerLettersEnc = defaultFormatPreservingEncryption1.encrypt(lowerLetters.toString(), anyTweak);
+        } else {
+        	lowerLettersEnc = lowerLetters.toString();
+        }
+        
+        String capLettersEnc;
+        if (capLetters.length() > 1) {
+        	capLettersEnc = capLettersFormatPreservingEncryption.encrypt(capLetters.toString(), anyTweak);
+        } else {
+        	capLettersEnc = capLetters.toString();
+        }
+        
+        System.out.println("Numbers Encoded: " + numbersEnc);
+        System.out.println("Lower Letters Encoded: " + lowerLettersEnc);
+        System.out.println("Cap Letters Encoded: " + capLettersEnc);
+        
+        Character[] charNumbers = numbersEnc.chars().mapToObj(ch -> (char) ch).toArray(Character[]::new);
+        Queue<Character> queueNumbers = new LinkedList<Character>(Arrays.asList(charNumbers));
+        
+        Character[] charLowerLetters = lowerLettersEnc.chars().mapToObj(ch -> (char) ch).toArray(Character[]::new);
+        Queue<Character> lowerQueueLetters = new LinkedList<Character>(Arrays.asList(charLowerLetters));
+        
+        Character[] charCapLetters = capLettersEnc.chars().mapToObj(ch -> (char) ch).toArray(Character[]::new);
+        Queue<Character> capQueueLetters = new LinkedList<Character>(Arrays.asList(charCapLetters));
+        
+        StringBuilder fpeFinal = new StringBuilder(input);
+        
+        for (int i = 0; i < input.length(); i++){
+        	Character currChar = input.charAt(i);
+            if ((Character.isLetter(currChar)) && (Character.isLowerCase(currChar))) {
+            	fpeFinal.setCharAt(i, lowerQueueLetters.poll());
+            } else if ((Character.isLetter(currChar)) && (!Character.isLowerCase(currChar))) {
+                fpeFinal.setCharAt(i, capQueueLetters.poll());
+            } else if (Character.isDigit(currChar)) {
+            	fpeFinal.setCharAt(i, queueNumbers.poll());
+            } else {
+            	fpeFinal.setCharAt(i, queueOthers.poll());
+            }
+        }
+        
+        System.out.println();
+        System.out.println("FPE Final Text Encoded: " + fpeFinal);
+        
+        return fpeFinal.toString();
+    }
 
 }
--- format-preserving-encryption/src/test/java/com/jds/fpe/junit/SparkUDFTest.java
+++ format-preserving-encryption/src/test/java/com/jds/fpe/junit/SparkUDFTest.java
@@ -2,6 +2,16 @@ package com.jds.fpe.junit;
 
 import static org.apache.spark.sql.functions.callUDF;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
@@ -9,50 +19,93 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.api.java.UDF1;
 import org.apache.spark.sql.types.DataTypes;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.holdenkarau.spark.testing.JavaDatasetSuiteBase;
+import com.jds.fpe.Encryption;
 
 //import com.holdenkarau.spark.testing.SharedJavaSparkContext;
 
 //public class SparkUDFTest extends SharedJavaSparkContext{
 
-public class SparkUDFTest {
+public class SparkUDFTest extends JavaDatasetSuiteBase implements Serializable {
 	
-//	@Test
-//	public void testSparkFPEUDF() {
-//		SparkSession spark = SparkSession.builder().appName("FPE").master("local").getOrCreate();
-//
-//		// registers a new internal UDF
-//		spark.udf().register("encrypt", new UDF1<Integer, Integer>() {
-//			private static final long serialVersionUID = -5372447039252716846L;
-//
-//			@Override
-//			public Integer call(Integer x) {
-//				return x * 2;
-//			}
-//		}, DataTypes.IntegerType);
-//
-//		String filename = "data/encryption-test-file.csv";
-//		Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true").option("header", "false")
-//				.load(filename);
-//		df = df.withColumn("label", df.col("_c0")).drop("_c0");
-//		df = df.withColumn("value", df.col("_c1")).drop("_c1");
-//		df = df.withColumn("x2", callUDF("internal", df.col("value").cast(DataTypes.IntegerType)));
-//		df.show();
-//	}
+	  private Configuration conf;
+	  private FileSystem fs;
+	  private MiniDFSCluster cluster;
+	  private JavaSparkContext sparkContext;
+	  private SparkSession spark;
+	  private SparkConf sconf;
+	  
+	  @BeforeClass
+	  public static void setupTests() throws Exception {
+	    // Force logging level for a job class
+//	    LogManager.getLogger(SparkUDFTest.class).setLevel(Level.DEBUG);
+	  }
+	  
+	  @Before
+	  public void setup() throws Exception{
+	        SparkConf sconf = new SparkConf()
+	                .setMaster("local[*]")
+	                .setAppName("FPE")
+	                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+	                .set("spark.io.compression.codec", "lz4")
+	                .set("spark.acls.enable", "false")
+	                .set("spark.ui.enabled", "false");
+
+	        spark = SparkSession
+	                .builder()
+	                .appName("FPE")
+	                .config(sconf)
+	                .getOrCreate();
+	  }
 	
     @Test
-    public void testSparkFPEUDF() {
-        SparkConf conf = new SparkConf()
-                .setMaster("local[*]")
-                .setAppName("FPE")
-                .set("fs.s3.canned.acl","BucketOwnerFullControl")
-                .set("spark.acls.enable", "false")
-                .set("spark.ui.enabled", "false");
+    public void testSparkFPEUDF() throws Exception{
+        
+		// Registers a new internal UDF
+        spark.udf().register("encryptStr", new UDF1<String, String>() {
+			private static final long serialVersionUID = -5372447039252716846L;
 
-        JavaSparkContext jsc = new JavaSparkContext(conf);
-        jsc.hadoopConfiguration().set("fs.s3.canned.acl","BucketOwnerFullControl" );
-        // use it
-        jsc.stop();
+			@Override
+			public String call(String x) {
+				Encryption encryption = new Encryption();
+		        return encryption.encryptStr(x);
+			}
+		}, DataTypes.StringType);
+
+        spark.udf().register("encryptInt", new UDF1<Integer, Integer>() {
+			private static final long serialVersionUID = -5372447039252716846L;
+
+			@Override
+			public Integer call(Integer x) {
+				
+				Encryption encryption = new Encryption();
+		        return encryption.encryptInt(x);
+			}
+		}, DataTypes.IntegerType);
+
+		String filename = "data/encryption-test-file.csv";
+		Dataset<Row> df = spark.read().format("csv").option("inferSchema", "true").option("header", "false")
+				.load(filename);
+		df = df.withColumn("label", df.col("_c0")).drop("_c0");
+		df = df.withColumn("integer", df.col("_c1")).drop("_c1");
+		df = df.withColumn("string", df.col("_c2")).drop("_c2");
+		df = df.withColumn("encInt", callUDF("encryptInt", df.col("integer").cast(DataTypes.IntegerType)));
+		df = df.withColumn("encStr", callUDF("encryptStr", df.col("string").cast(DataTypes.StringType)));
+		df.show();    
+	
+    }
+    
+    @After
+    public void tearDown() {
+	    if (spark != null) {
+	    	spark.stop();
+	    	spark = null;
+	      }
     }
 
 

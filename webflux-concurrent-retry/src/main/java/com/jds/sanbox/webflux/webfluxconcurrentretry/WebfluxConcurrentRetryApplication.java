f2913c170f34d144f70b622edf1d2df60bb0f907
Daniel Sobrado <daniel@danielsobrado.com>
2022-12-01 11:32:26 +0400

    Initial

---
+++ webflux-concurrent-retry/src/main/java/com/jds/sanbox/webflux/webfluxconcurrentretry/WebfluxConcurrentRetryApplication.java
@@ -0,0 +1,13 @@
+package com.jds.sanbox.webflux.webfluxconcurrentretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxConcurrentRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxConcurrentRetryApplication.class, args);
+	}
+
+}
